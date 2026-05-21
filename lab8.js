d6a3eba8ea66655e5a84c77b12a632a5b8e245dd
Yelyzaveta Sh <yelyzavetash@MacBook-Air-Yelyzaveta.local>
2026-05-21 22:47:59 +0300

    Implement authentication header injection

--- lab8.js
+++ lab8.js
@@ -10,3 +10,29 @@ setAuthentication(type, credentials) {
   this.authType = type;
   this.authData = credentials;
 }
+
+buildHeaders() {
+  const headers = {};
+
+  switch (this.authType) {
+    case "API_KEY":
+      headers["x-api-key"] =
+        this.authData.apiKey;
+      break;
+
+    case "JWT":
+      headers["Authorization"] =
+        `Bearer ${this.authData.token}`;
+      break;
+
+    case "OAUTH":
+      headers["Authorization"] =
+        `OAuth ${this.authData.token}`;
+      break;
+
+    default:
+      break;
+  }
+
+  return headers;
+}
